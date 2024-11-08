4e2551fdad17101c21691e0fd732751042bce5fb
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2024-11-08 17:37:29 +0530

    testing of new components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Heading from '@/components/Heading'
+import TextField from '@/components/TextField'
+import { useState } from 'react'
 
 const Index = () => {
+  const [value, setValue] = useState("")
+
   return (
-    <div>Index</div>
+    <div className='flex flex-col gap-2'>
+      <Heading text="Hello World" />
+      <TextField value={value} onChange={(val) => setValue(val)} />
+    </div>
   )
 }
 
