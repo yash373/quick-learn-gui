5a026d18be12a4c21bd7181a05f39843bddd0e4e
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2024-11-08 17:45:11 +0530

    Integrated with desired inputs for backend to work and generate outputs

--- components/TextField.tsx
+++ components/TextField.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 interface TextFieldProps {
-  value: string;
-  onChange: (val: string) => void;
+  value: string,
+  placeholder?: string,
+  onChange: (val: string) => void
 }
 
-const TextField = ({ value, onChange }: TextFieldProps) => {
+const TextField = ({ value, onChange, placeholder }: TextFieldProps) => {
   return (
     <input
+      placeholder={placeholder}
       value={value}
       onChange={({ target: { value } }) => onChange(value)}
     />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,18 @@ import TextField from '@/components/TextField'
 import { useState } from 'react'
 
 const Index = () => {
-  const [value, setValue] = useState("")
+  const [exam, setExam] = useState("")
+  const [syllabus, setSyllabus] = useState("")
+  const [time, setTime] = useState("")
+  const [context, setContext] = useState("")
 
   return (
     <div className='flex flex-col gap-2'>
-      <Heading text="Hello World" />
-      <TextField value={value} onChange={(val) => setValue(val)} />
+      <Heading text="Quick Learn" />
+      <TextField placeholder='Exam Name: (ex: SST Pre-board)' value={exam} onChange={(val) => setExam(val)} />
+      <TextField placeholder='Syllabus: (ex: SST Pre-board)' value={syllabus} onChange={(val) => setSyllabus(val)} />
+      <TextField placeholder='Time: (ex: 9 days)' value={time} onChange={(val) => setTime(val)} />
+      <TextField placeholder='Context: (ex: I am bad at history)' value={context} onChange={(val) => setContext(val)} />
     </div>
   )
 }
