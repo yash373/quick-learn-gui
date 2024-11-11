764c2b28ea8e2b55d3219e80c93ab8c9432f6487
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2024-11-11 19:18:30 +0530

    Added Sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://quick-learn-gui.vercel.app/', // Change this to your site's URL
+    generateRobotsTxt: true, // (Optional) Generate robots.txt file
+    // Additional options
+};  
